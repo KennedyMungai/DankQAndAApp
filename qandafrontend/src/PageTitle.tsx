7f8449fe4869bb1a962f5f781fe4eeb7a124c55a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-11 15:43:41 +0300

    Created the PageTitle react component with some skeleton code

---
+++ qandafrontend/src/PageTitle.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const PageTitle = () => {
+  return (
+    <div>PageTitle</div>
+  )
+}
+
+export default PageTitle
