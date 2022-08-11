d1b03c1213b39e0e55adcc6f348bccf24de8741a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-11 15:19:00 +0300

    Added the Question react component and passed on the question parameter

--- qandafrontend/src/QuestionList.tsx
+++ qandafrontend/src/QuestionList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { QuestionData } from './QuestionsData'
+import { Question } from './Question'
 
 interface Props {
     data: QuestionData[];
@@ -9,7 +10,9 @@ const QuestionList = ({ data }: Props) => {
   return (
     <ul>
         {data.map((question) => (
-            <li key={question.questionId}></li>
+            <li key={question.questionId}>
+              <Question data={question}/>
+            </li>
         ))}
     </ul>
   )
