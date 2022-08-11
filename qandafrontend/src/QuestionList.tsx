f81914dd110276ec598c834abeaea3eeb7eda808
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-11 14:13:32 +0300

    Created an interface to hold the props

--- qandafrontend/src/QuestionList.tsx
+++ qandafrontend/src/QuestionList.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { QuestionData } from './QuestionsData'
 
+interface Props {
+    data: QuestionData[];
+}
+
 const QuestionList = () => {
   return (
     <div>QuestionList</div>
