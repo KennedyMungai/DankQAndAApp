e06abfc89b79523e72d92eb11ccbade6b01f1a8c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-11 15:47:51 +0300

    Create dthe Page component to iterate over the PageTitle components

---
+++ qandafrontend/src/Page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PageTitle from './PageTitle'
+
+interface Props {
+    title?: string;
+    children: React.ReactNode;
+}
+
+const Page = ({title, children}: Props) => {
+  return (
+    <div>
+        {title && <PageTitle>{title}</PageTitle>}
+        {children}
+    </div>
+  )
+}
+
+export default Page
