1e800d61bf1f16073bbf0c640ba6eaf8b912541e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-11 20:09:09 +0300

    Added a useEffect hook

--- qandafrontend/src/HomePage.tsx
+++ qandafrontend/src/HomePage.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Page from './Page'
 import PageTitle from './PageTitle'
 import QuestionList from './QuestionList'
 import { getUnansweredQuestions } from './QuestionsData'
 
 const HomePage = () => {
+  useEffect(() => {
+    console.log('first rendered')
+  }, []);
+
   return (
     <Page>
       <div>
