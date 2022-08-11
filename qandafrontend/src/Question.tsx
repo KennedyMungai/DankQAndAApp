082bd0071d6a62b6e6c167e53f9cd14f1d19d08a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-11 15:32:05 +0300

    Added conditional code in the second div to cut the number of displayed characters when the question is longer than 50 characters

--- qandafrontend/src/Question.tsx
+++ qandafrontend/src/Question.tsx
@@ -13,6 +13,13 @@ const Question = ({ data }: Props) => {
         </div>
         <div>
             {
+                data.content.length > 50
+                ? `${data.content.substring(0, 50)}...`
+                : data.content
+            }
+        </div>
+        <div>
+            {
                 `Asked by ${data.userName} on
                 ${data.created.toLocaleDateString()}
                 ${data.created.toLocaleTimeString()}`
