3479564b24c3fdd6c806a3fa38d0f0879e3faa17
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-11 12:43:27 +0300

    Added the input tag

--- qandafrontend/src/Header.tsx
+++ qandafrontend/src/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <a href="./">Q & A</a>
-  )
+    return (
+        <>
+            <a href="./">Q & A</a>
+            <input type="text" name="" id="" placeholder="Search..." />
+        </>
+    )
 }
 
 export default Header
