c1e43e370ae85d1f8b32b441707703a508790571
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-11 14:12:42 +0300

    Created the QuestionList react component

---
+++ qandafrontend/src/QuestionList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { QuestionData } from './QuestionsData'
+
+const QuestionList = () => {
+  return (
+    <div>QuestionList</div>
+  )
+}
+
+export default QuestionList
