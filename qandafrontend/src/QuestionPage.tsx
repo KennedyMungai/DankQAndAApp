c2347140981dcbd700b07333bb90cd239be10127
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 11:44:28 +0300

    Assigned the useParams to an anonymous variable

--- qandafrontend/src/QuestionPage.tsx
+++ qandafrontend/src/QuestionPage.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import { Page } from './Page'
 
 const QuestionPage = () => {
+    const { questionId } = useParams();
+
   return (
     <Page>Question Page</Page>
   )
