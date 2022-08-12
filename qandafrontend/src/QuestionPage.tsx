8e1a2d606e0757db8f18400a9b0d934ed77b050a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 12:02:46 +0300

    Added a useEffect react hook for whenever the questionId changes

--- qandafrontend/src/QuestionPage.tsx
+++ qandafrontend/src/QuestionPage.tsx
@@ -1,15 +1,26 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from '@emotion/react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Page } from './Page'
-import { QuestionData } from './QuestionsData'
+import { getQuestion, QuestionData } from './QuestionsData'
 
 const QuestionPage = () => {
     const [question, setQuestion] = useState<QuestionData | null>(null)
-
     const { questionId } = useParams();
+
+    useEffect(() => {
+      const doGetQuestion = async (questionId: number) => {
+        const foundQuestion = await getQuestion(questionId);
+        setQuestion(foundQuestion);
+      }
+    
+      if (questionId) {
+        doGetQuestion(Number(questionId));
+      }
+    }, [questionId])
+    
 
   return (
     <Page>Question Page {questionId}</Page>
