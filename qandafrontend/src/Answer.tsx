a34bfea09993b8123cf1dd32b4dd321b426a849f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 12:32:13 +0300

    Added dom logic for the answer component

--- qandafrontend/src/Answer.tsx
+++ qandafrontend/src/Answer.tsx
@@ -8,9 +8,35 @@ interface Props {
     data: AnswerData;
 }
 
-const Answer = () => {
+const Answer = ({ data }: Props) => {
   return (
-    <div>Answer</div>
+    <div css={
+        css`
+            padding: 10px 0px;
+        `
+    }>
+        <div css={
+            css`
+                padding: 10px 0px;
+                font-size: 13px;
+            `
+        }>
+            {data.content}
+        </div>
+        <div css={
+            css`
+                font-size: 12px;
+                font-style: italic;
+                color: ${gray3};
+            `
+        }>
+            {`
+                Answered by ${data.userName} on 
+                ${data.created.toLocaleDateString()}
+                ${data.created.toLocaleTimeString()}
+            `}
+        </div>
+    </div>
   )
 }
 
