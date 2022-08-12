7d878c26bfaccfb0466e14e7d80cc55dd00d4d80
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 12:20:13 +0300

    Created the skeleton code for the component

---
+++ qandafrontend/src/AnswerList.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import React from 'react'
+import { AnswerData } from './QuestionsData'
+import { Answer } from './Answer'
+import { gray5 } from './Styles'
+
+const AnswerList = () => {
+  return (
+    <div>AnswerList</div>
+  )
+}
+
+export default AnswerList
