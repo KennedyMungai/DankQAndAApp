6e8d412ba5ed9833c3b489eaae450cd6f802c47e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 12:27:34 +0300

    Added teh skeleton code for the component

---
+++ qandafrontend/src/Answer.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import React from 'react'
+import { AnswerData } from './QuestionsData'
+import { gray3 } from './Styles'
+
+const Answer = () => {
+  return (
+    <div>Answer</div>
+  )
+}
+
+export default Answer
