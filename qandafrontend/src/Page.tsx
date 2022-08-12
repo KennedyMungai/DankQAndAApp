3f6c0c0139ad238b9211cea3cb5706217cd090da
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 10:37:27 +0300

    Commented out the offending code

--- qandafrontend/src/Page.tsx
+++ qandafrontend/src/Page.tsx
@@ -12,13 +12,13 @@ interface Props {
 const Page = ({title, children}: Props) => {
   return (
     <div 
-      css={
-        css`
-          margin: 50px auto 20px auto;
-          padding: 30px 20px;
-          max-width: 600px;
-        `
-      }
+      // css={
+      //   css`
+      //     margin: 50px auto 20px auto;
+      //     padding: 30px 20px;
+      //     max-width: 600px;
+      //   `
+      // }
     >
         {title && <PageTitle>{title}</PageTitle>}
         {children}
