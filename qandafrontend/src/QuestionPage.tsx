62a5d063b3bd3df98a74078b92342fe0460ee172
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 11:58:42 +0300

    Added a question useState hook

--- qandafrontend/src/QuestionPage.tsx
+++ qandafrontend/src/QuestionPage.tsx
@@ -1,11 +1,14 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from '@emotion/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Page } from './Page'
+import { QuestionData } from './QuestionsData'
 
 const QuestionPage = () => {
+    const [question, setQuestion] = useState<QuestionData | null>(null)
+
     const { questionId } = useParams();
 
   return (
