026ba18e736b27bc4c4e23257978e615905d7d79
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 11:22:19 +0300

    Added basic skeleton code to the component file

---
+++ qandafrontend/src/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Page } from './Page'
+
+const NotFoundPage = () => {
+  return (
+    <Page title='Page Not Found'>{null}</Page>
+  )
+}
+
+export default NotFoundPage
