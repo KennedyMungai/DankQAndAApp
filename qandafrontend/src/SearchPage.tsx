ffe461fb23e3c9f114a23bd88924f3cba02f9394
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 14:18:29 +0300

    Added import statements to the SearchPage

--- qandafrontend/src/SearchPage.tsx
+++ qandafrontend/src/SearchPage.tsx
@@ -1,5 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 import { Page } from './Page'
+import QuestionList from './QuestionList'
+import { searchQuestions, QuestionData } from './QuestionsData'
 
 const SearchPage = () => {
   return (
