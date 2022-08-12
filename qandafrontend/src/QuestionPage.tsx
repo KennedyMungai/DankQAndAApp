0635ab6a09450f8d0111b5d16db4b58a806f7971
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 12:08:12 +0300

    Rewrote the rendering code for the title with its own custom styling

--- qandafrontend/src/QuestionPage.tsx
+++ qandafrontend/src/QuestionPage.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Page } from './Page'
 import { getQuestion, QuestionData } from './QuestionsData'
+import { gray6 } from './Styles'
 
 const QuestionPage = () => {
     const [question, setQuestion] = useState<QuestionData | null>(null)
@@ -22,7 +23,27 @@ const QuestionPage = () => {
     }, [questionId])
 
   return (
-    <Page>Question Page {questionId}</Page>
+    <Page>
+        <div css={
+            css`
+                background-color: white;
+                padding: 15px 20px 20px 20px;
+                border-radius: 4px;
+                border: 1px solid ${gray6};
+                box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+            `
+        }>
+            <div css={
+                css`
+                    font-size: 19px;
+                    font-weight: bold;
+                    margin: 10px 0px 5px;
+                `
+            }>
+                {question == null ? '' : question.title}
+            </div>
+        </div>
+    </Page>
   )
 }
 
