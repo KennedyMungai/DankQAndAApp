d93500cbf196db113846f5cd91e43933ed6c25b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 10:29:31 +0300

    Added an import from the emotion/styled css class

--- qandafrontend/src/Styles.ts
+++ qandafrontend/src/Styles.ts
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled'
+
 export const gray1 = '#383737';
 export const gray2 = '#5c5a5a';
 export const gray3 = '#857c81';
@@ -13,3 +15,4 @@ export const accent2 = '#efd197';
 
 export const fontFamily = "'Segoe UI, Tahoma, Geneva, Verdana, sans-serif'";
 export const fontSize = '16px';
+
