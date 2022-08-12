8643992b2a057065a3f02e6d4fc87b22abf5bacc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-12 12:28:22 +0300

    Created the Props interface with one data field

--- qandafrontend/src/Answer.tsx
+++ qandafrontend/src/Answer.tsx
@@ -4,6 +4,10 @@ import React from 'react'
 import { AnswerData } from './QuestionsData'
 import { gray3 } from './Styles'
 
+interface Props {
+    data: AnswerData;
+}
+
 const Answer = () => {
   return (
     <div>Answer</div>
