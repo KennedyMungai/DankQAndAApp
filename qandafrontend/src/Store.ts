1b17474a4481a26697bc9b7dda94ffb067a9af9a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-13 12:20:09 +0300

    Created the QuestionsState interface

--- qandafrontend/src/Store.ts
+++ qandafrontend/src/Store.ts
@@ -1,1 +1,8 @@
 import { QuestionData } from "./QuestionsData";
+
+interface QuestionsState {
+    readonly loading: boolean;
+    readonly unanswered: QuestionData[];
+    readonly viewing: QuestionData | null;
+    readonly searched: QuestionData[];
+}
