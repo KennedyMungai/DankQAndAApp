afe782bd5b8ce3acb1e7598d9153dfaa245a42cd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-13 19:08:05 +0300

    Added imports for the useSelector and the useDispatch react redux hooks

--- qandafrontend/src/SearchPage.tsx
+++ qandafrontend/src/SearchPage.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { useSearchParams } from 'react-router-dom'
 import { Page } from './Page'
 import QuestionList from './QuestionList'
