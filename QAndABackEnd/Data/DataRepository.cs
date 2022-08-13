0e05e64544c00ff1e134010b3a5c704d51c9da70
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-13 21:21:36 +0300

    Implemented the IDataRepository interface

--- QAndABackEnd/Data/DataRepository.cs
+++ QAndABackEnd/Data/DataRepository.cs
@@ -1,6 +1,34 @@
 namespace QAndABackEnd.Data;
 
-public class DataRepository
+public class DataRepository : IDataRepository
 {
-    
+    public AnswerGetResponse GetAnswer(int answerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public QuestionGetSingleResponse GetQuestion(int questionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<QuestionGetManyResponse> GetQuestions()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool QuestionExists(int questionId)
+    {
+        throw new NotImplementedException();
+    }
 }
