3a09fdff89d72e6ed77172eda63a897c4c98ba1c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-13 21:20:33 +0300

    Created the template for the Data respository

--- QAndABackEnd/Data/IDataRepository.cs
+++ QAndABackEnd/Data/IDataRepository.cs
@@ -2,5 +2,10 @@ namespace QAndABackEnd.Data;
 
 public interface IDataRepository
 {
-    
+    IEnumerable<QuestionGetManyResponse> GetQuestions();
+    IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
+    IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
+    QuestionGetSingleResponse GetQuestion(int questionId);
+    bool QuestionExists(int questionId);
+    AnswerGetResponse GetAnswer(int answerId);
 }
