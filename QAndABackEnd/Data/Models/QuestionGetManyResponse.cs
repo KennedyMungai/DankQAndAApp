53e9c941197f63610a9e521f1ae959cb62dc7bbf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-13 21:37:34 +0300

    Added some data fields to the model class

--- QAndABackEnd/Data/Models/QuestionGetManyResponse.cs
+++ QAndABackEnd/Data/Models/QuestionGetManyResponse.cs
@@ -2,5 +2,9 @@ namespace QAndABackEnd.Data.Models;
 
 public class QuestionGetManyResponse
 {
-    
+    public int QuestionId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string UserName { get; set; }
+    public DateTime Created { get; set; }
 }
