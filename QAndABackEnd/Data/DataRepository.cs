1c6c159e16702a7ee4e5d7b1604653ecf7ac8a38
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-13 22:13:19 +0300

    Added database code to teh GetAnswer function

--- QAndABackEnd/Data/DataRepository.cs
+++ QAndABackEnd/Data/DataRepository.cs
@@ -15,7 +15,13 @@ public class DataRepository : IDataRepository
 
     public AnswerGetResponse GetAnswer(int answerId)
     {
-        throw new NotImplementedException();
+        using var connection = new SqlConnection(_connectionString);
+        connection.Open();
+        return connection.QueryFirstOrDefault<AnswerGetResponse>(
+            @"EXEC dbo.Answer_Get_ByAnswerId 
+            @AnswerId = @AnswerId",
+            new { AnswerId = answerId }
+        );
     }
 
     public QuestionGetSingleResponse GetQuestion(int questionId)
