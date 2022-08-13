5bf2d734eae288b2efafe96d5c48e20412e45fe7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-13 09:29:34 +0300

    Added styling code to the file

--- qandafrontend/src/AskPage.tsx
+++ qandafrontend/src/AskPage.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import { Page } from './Page'
+import { 
+    FieldSet, 
+    FieldContainer, 
+    FieldLabel, 
+    FieldInput, 
+    FieldTextArea, 
+    FormButtonContainer, 
+    PrimaryButton } from './Styles'
+
 
 const AskPage = () => {
   return (
