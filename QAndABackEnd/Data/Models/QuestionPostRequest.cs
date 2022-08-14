a0e8d36527ea8b877fc86c5c3eecd12352c34956
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-14 10:49:41 +0300

    Added the basic fields to the model class

---
+++ QAndABackEnd/Data/Models/QuestionPostRequest.cs
@@ -0,0 +1,10 @@
+namespace QAndABackEnd.Data.Models;
+
+public class QuestionPostRequest
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public DateTime Created { get; set; }
+}
