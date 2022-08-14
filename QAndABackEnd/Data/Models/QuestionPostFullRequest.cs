b2cd06b8054766e3ef3d720faf387f9572765de2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-14 20:34:26 +0300

    Created the QuestionPostFullRequest model class

---
+++ QAndABackEnd/Data/Models/QuestionPostFullRequest.cs
@@ -0,0 +1,10 @@
+namespace QAndABackEnd.Data.Models;
+
+public class QuestionPostFullRequest
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public DateTime Created { get; set; }
+}
