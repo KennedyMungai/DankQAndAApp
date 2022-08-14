267b31fc16d29823803255f4a3125a331dd804b6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-14 11:07:53 +0300

    Added the connection string as a variable

--- QAndABackEnd/Program.cs
+++ QAndABackEnd/Program.cs
@@ -1,3 +1,5 @@
+using DbUp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 var app = builder.Build();
 
