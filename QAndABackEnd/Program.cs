2a9d11e8d5c63f72f9c6679ad63a27a03c0be40e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-14 11:40:18 +0300

    Added the Data repositories to the services

--- QAndABackEnd/Program.cs
+++ QAndABackEnd/Program.cs
@@ -1,5 +1,6 @@
 using System.Security.AccessControl;
 using DbUp;
+using QAndABackEnd.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDataRepository, DataRepository>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 EnsureDatabase.For.SqlDatabase(connectionString);
 var upgrader = DeployChanges.To
