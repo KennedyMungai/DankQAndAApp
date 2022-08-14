b3e1f2594019880c591c58393127c4d5337e3b64
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-14 11:03:27 +0300

    Created the AnswerPostRequest model class

---
+++ QAndABackEnd/Data/Models/AnswerPostRequest.cs
@@ -0,0 +1,10 @@
+namespace QAndABackEnd.Data.Models;
+
+public class AnswerPostRequest
+{
+    public int QuestionId { get; set; }
+    public string Content { get; set; }
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public DateTime Created { get; set; }
+}
