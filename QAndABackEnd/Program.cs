ba864d103e364bbe2bd792e32c96abde04b8f2e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-15 18:35:17 +0300

    Registered the QuestionCache service

--- QAndABackEnd/Program.cs
+++ QAndABackEnd/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IQuestionCache, QuestionCache>();
 builder.Services.AddScoped<IDataRepository, DataRepository>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 EnsureDatabase.For.SqlDatabase(connectionString);
