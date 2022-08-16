7fb79146df08740bf3791ed46aca3b35dbe2c603
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 16:01:32 +0300

    Put the return statement in  the AuthProvider components

--- qandafrontend/src/App.tsx
+++ qandafrontend/src/App.tsx
@@ -20,47 +20,49 @@ const AskPage = React.lazy(() => import('./AskPage'));
 const store = configureStore();
 
 function App() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div css={
-          css`
-            font-family: ${fontFamily};
-            font-size: ${fontSize};
-            color: ${gray2};
-          `
-        }>
-          <Header />
-          <Routes>
-            <Route path="" element={<HomePage/>} />
-            <Route path="search" element={<SearchPage/>} />
-
-            <Route path="ask" element={
-              <Suspense fallback={
-                <div css={
-                  css`
-                    margin-top: 100px;
-                    text-align: center;
-                  `
-                }>
-                  Loading...
-                </div>
-              }>
-                <AskPage />
-              </Suspense>
-            } />
-
-            <Route path="signin" element={<SignInPage action="signin" />} />
-            <Route path='/signin-callback' element={<SignInPage action="signin-callback" />} />
-            <Route path='signout' element={<SignOutPage action="signout" />} />
-            <Route path='/signout-callback' element={ <SignOutPage action="signout-callback" /> } />
-            <Route path="*" element={<NotFoundPage/>} />
-            <Route path="questions/:questionId" element={<QuestionPage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </Provider>
-  );
+  <AuthProvider>
+      return (
+        <Provider store={store}>
+          <BrowserRouter>
+            <div css={
+              css`
+                font-family: ${fontFamily};
+                font-size: ${fontSize};
+                color: ${gray2};
+              `
+            }>
+              <Header />
+              <Routes>
+                <Route path="" element={<HomePage/>} />
+                <Route path="search" element={<SearchPage/>} />
+    
+                <Route path="ask" element={
+                  <Suspense fallback={
+                    <div css={
+                      css`
+                        margin-top: 100px;
+                        text-align: center;
+                      `
+                    }>
+                      Loading...
+                    </div>
+                  }>
+                    <AskPage />
+                  </Suspense>
+                } />
+    
+                <Route path="signin" element={<SignInPage action="signin" />} />
+                <Route path='/signin-callback' element={<SignInPage action="signin-callback" />} />
+                <Route path='signout' element={<SignOutPage action="signout" />} />
+                <Route path='/signout-callback' element={ <SignOutPage action="signout-callback" /> } />
+                <Route path="*" element={<NotFoundPage/>} />
+                <Route path="questions/:questionId" element={<QuestionPage />} />
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </Provider>
+      );
+  </AuthProvider>
 }
 
 export default App;
