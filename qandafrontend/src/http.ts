56f10b1a8c4e7bcd6e64c696339c72096b234caa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 12:55:10 +0300

    Created the http variable

--- qandafrontend/src/http.ts
+++ qandafrontend/src/http.ts
@@ -8,3 +8,7 @@ export interface HttpResponse<RESB> {
     ok: boolean;
     body?: RESB;
 }
+
+export const http = async <RESB, REQB = undefined>(config: HttpRequest<REQB>) : Promise<HttpResponse<RESB>> => {
+
+};
