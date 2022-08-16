91c91fdd27bab317dacd9cafb2b3bdbf5cecd89d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 20:22:27 +0300

    Created the Props interface which has the Props fiedl of type SigninAction

--- qandafrontend/src/SignInPage.tsx
+++ qandafrontend/src/SignInPage.tsx
@@ -5,6 +5,10 @@ import { useAuth } from './Auth'
 
 type SigninAction = 'signin' | 'signin-callback';
 
+interface Props {
+  action: SigninAction;
+}
+
 const SignInPage = () => {
   return (
     <Page title='Sign In'>{null}</Page>
