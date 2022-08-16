6cae5d7edd5ee6b285550a301fc840b56611537c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 11:08:47 +0300

    Added the Http client service to the application

--- QAndABackEnd/Program.cs
+++ QAndABackEnd/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 builder.Services.AddSingleton<IQuestionCache, QuestionCache>();
 builder.Services.AddScoped<IDataRepository, DataRepository>();
 builder.Services.AddAuthentication(options =>
