667e79101bd706745638e7edb5e0b78d74025109
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 20:21:29 +0300

    Created the SigninAction type

--- qandafrontend/src/SignInPage.tsx
+++ qandafrontend/src/SignInPage.tsx
@@ -3,6 +3,8 @@ import { Page } from './Page'
 import { StatusText } from './Styles'
 import { useAuth } from './Auth'
 
+type SigninAction = 'signin' | 'signin-callback';
+
 const SignInPage = () => {
   return (
     <Page title='Sign In'>{null}</Page>
