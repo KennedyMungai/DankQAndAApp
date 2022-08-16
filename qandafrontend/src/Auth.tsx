13b5a5b44aa4c4194f22ce527795b44fe7bee0d9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 15:32:14 +0300

    Set the isAuthenticated, user , authooclient and loading states

--- qandafrontend/src/Auth.tsx
+++ qandafrontend/src/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import createAuth0Client from "@auth0/auth0-spa-js";
 import Auth0Client from "@auth0/auth0-spa-js/dist/typings/Auth0Client";
 import { authSettings } from './AppSettings';
@@ -24,3 +24,10 @@ export const Auth0Context = createContext<IAuth0Context>({
 });
 
 export const useAuth = () => useContext(Auth0Context);
+
+export const AuthProvider: React.FC = ({ children, }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [user, setUser] = useState<Auth0User | undefined>(undefined);
+    const [auth0Client, setAuth0Client] = useState<Auth0Client>()
+    const [loading, setLoading] = useState<boolean>(true)
+};
