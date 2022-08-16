fbd6da6f42197ca2b0deafb219b4b02b62a63f9b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 15:48:03 +0300

    Used the useEffect hook to assign values to the various state values

--- qandafrontend/src/Auth.tsx
+++ qandafrontend/src/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import createAuth0Client from "@auth0/auth0-spa-js";
 import Auth0Client from "@auth0/auth0-spa-js/dist/typings/Auth0Client";
 import { authSettings } from './AppSettings';
@@ -25,11 +25,41 @@ export const Auth0Context = createContext<IAuth0Context>({
 
 export const useAuth = () => useContext(Auth0Context);
 
-export const AuthProvider: React.FC = ( children ) => {
+export const AuthProvider: React.FC = ( {children}: any ) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [user, setUser] = useState<Auth0User | undefined>(undefined);
     const [auth0Client, setAuth0Client] = useState<Auth0Client>()
     const [loading, setLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+      const initAuth0 = async () => {
+        setLoading(true);
+        const auth0FromHook = await createAuth0Client(authSettings);
+        setAuth0Client(auth0FromHook);
+        
+        const isAuthenticatedFromHook = await auth0FromHook.isAuthenticated();
+
+        if (isAuthenticatedFromHook) {
+            const user = await auth0FromHook.getUser();
+            setUser(user);
+        }
+
+        setIsAuthenticated(isAuthenticatedFromHook);
+        setLoading(false);
+      }
+    
+      initAuth0();
+    }, [])
+    
+
+    const getAuth0ClientFromState = () => {
+        if(auth0Client === undefined)
+        {
+            throw new Error('Auth0 client is not set');
+        }
+
+        return auth0Client;
+    }
 
     return (
         <Auth0Context.Provider value={{
