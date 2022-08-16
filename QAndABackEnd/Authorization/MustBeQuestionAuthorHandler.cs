b48c41e2ff72f44756a90031be9725b94166e4ac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 11:24:26 +0300

    Created the constructor and initialized IDataRepository and IHttpContextAccessor

--- QAndABackEnd/Authorization/MustBeQuestionAuthorHandler.cs
+++ QAndABackEnd/Authorization/MustBeQuestionAuthorHandler.cs
@@ -9,6 +9,15 @@ namespace QAndABackEnd.Authorization;
 
 public class MustBeQuestionAuthorHandler : AuthorizationHandler<MustBeQuestionAuthorRequirement>
 {
+    private readonly IDataRepository _dataRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public MustBeQuestionAuthorHandler(IDataRepository dataRepository, IHttpContextAccessor httpContextAccessor)
+    {
+        _dataRepository = dataRepository;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MustBeQuestionAuthorRequirement requirement)
     {
         throw new NotImplementedException();
