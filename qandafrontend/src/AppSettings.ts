d127db0e74a83dc3224e400139b650a92e96a15c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 20:42:27 +0300

    Updated the Auth0 stuff

--- qandafrontend/src/AppSettings.ts
+++ qandafrontend/src/AppSettings.ts
@@ -3,8 +3,8 @@ export const server = 'http://localhost:17525';
 export const webAPIUrl = `${server}/api`;
 
 export const authSettings = {
-  domain: 'your-tenantid.auth0.com',
-  client_id: 'your-clientid',
+  domain: 'dev-ehqqmkp1.auth0.com',
+  client_id: 'Pf5owdhL1lSI9J4x30Ajc0QIIScuFKEC',
   redirect_uri: window.location.origin + '/signin-callback',
   scope: 'openid profile QandAAPI email',
   audience: 'https://qanda',
