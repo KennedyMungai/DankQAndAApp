57c684528922fb4229d50c2e76ff71ab117a363f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-16 15:26:12 +0300

    Created the Auth.tsx component

---
+++ qandafrontend/src/Auth.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import createAuth0Client from "@auth0/auth0-spa-js";
+import Auth0Client from "@auth0/auth0-spa-js/dist/typings/Auth0Client";
+import { authSettings } from './AppSettings';
+
+interface Auth0User {
+    name: string;
+    email: string;
+}
+
+interface IAuth0Context {
+    isAuthenticated: boolean;
+    user?: Auth0User;
+    signIn: () => void;
+    signOut: () => void;
+    loading: boolean;
+}
+
+export const Auth0Context = React.createContext<IAuth0Context>({
+    isAuthenticated: false,
+    signIn: () => {},
+    signOut: () => {},
+    loading: true
+});
