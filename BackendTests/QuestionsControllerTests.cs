dba510b5dd3f98f928459252b93ce0dd7bc2dde3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-17 12:23:05 +0300

    Added the skeleton for the controller class

---
+++ BackendTests/QuestionsControllerTests.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+using Moq;
+using QAndABackEnd.Controllers;
+using QAndABackEnd.Data;
+using QAndABackEnd.Data.Models;
+
+namespace BackendTests;
+
+public class QuestionsControllerTests
+{
+    
+}
