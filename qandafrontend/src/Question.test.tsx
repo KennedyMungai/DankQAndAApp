fb509706facb55db27f141ab439edccadb792311
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-17 14:10:54 +0300

    Added a skeleton for the test

---
+++ qandafrontend/src/Question.test.tsx
@@ -0,0 +1,12 @@
+import { cleanup, render } from "@testing-library/react";
+import React from "react";
+import { QuestionData } from "./QuestionsData";
+import  Question  from './Question';
+import { BrowserRouter } from "react-router-dom";
+
+afterEach(cleanup);
+
+
+test('When the Question component is rendered, it should contain the correct data', () => { 
+    
+ });
