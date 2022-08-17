87f742b44dca005a048d053a27bf33ccc7163004
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-17 13:55:20 +0300

    Wrote a second test

--- qandafrontend/src/QuestionsData.test.ts
+++ qandafrontend/src/QuestionsData.test.ts
@@ -19,3 +19,33 @@ test('When mapQuestionFromServer is called with the question, created should be 
         answers:[]
     });
  });
+
+test('When mapQuestionFromServer is called with question and answers, created should be turned into a date', () => { 
+    const result = mapQuestionFromServer({
+        questionId: 1,
+        title: 'test',
+        content: 'test',
+        userName: 'test',
+        created: '2022-08-01T00:00:00.000Z',
+        answers: [{
+            answerId: 1,
+            content: 'test',
+            userName: 'test',
+            created: '2022-08-01T00:00:00.000Z'
+        }]
+    });
+
+    expect(result).toEqual({
+        questionId: 1,
+        title: 'test',
+        content: 'test',
+        userName: 'test',
+        created: new Date(Date.UTC(2022,0,1,0,0,0,0)),
+        answers: [{
+            answerId: 1,
+            content: 'test',
+            userName: 'test',
+            created: new Date(Date.UTC(2021,0,1,0,0,0,0))
+        }]
+    });
+ });
