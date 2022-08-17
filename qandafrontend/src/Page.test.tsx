16961c1618009a25326608bc33379d8a5c0a365a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-17 13:58:32 +0300

    Added the skeleton for the the test for the proper rendering of the Page component

---
+++ qandafrontend/src/Page.test.tsx
@@ -0,0 +1,7 @@
+import { cleanup, render } from '@testing-library/react'
+import React from 'react'
+import { Page } from './Page'
+
+test('When the Page component is rendered, it should contain the correct title and content', () => { 
+    
+ });
