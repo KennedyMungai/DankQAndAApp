2577b52623e7d9e9668a50585057a8858254c533
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-17 14:06:51 +0300

    Create dthe test for the Page component

--- qandafrontend/src/Page.test.tsx
+++ qandafrontend/src/Page.test.tsx
@@ -3,5 +3,17 @@ import React from 'react'
 import { Page } from './Page'
 
 test('When the Page component is rendered, it should contain the correct title and content', () => { 
-    
+    const { queryByText } = render (
+        <Page title='Title test'>
+            <span>Test content</span>
+        </Page>
+    );
+
+    const title = queryByText('Title test');
+    expect(title).not.toBeNull();
+
+    const content = queryByText('Test content');
+    expect(content).not.toBeNull();
+
+    afterEach(cleanup);
  });
