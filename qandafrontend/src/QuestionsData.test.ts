d9c120b4f737e7168841aabe6b79c3795006d59b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-17 13:47:47 +0300

    Created the test for turning the created variable into a date

--- qandafrontend/src/QuestionsData.test.ts
+++ qandafrontend/src/QuestionsData.test.ts
@@ -1,5 +1,21 @@
 import { mapQuestionFromServer } from './QuestionsData';
 
 test('When mapQuestionFromServer is called with the question, created should be turned into a date', () => { 
+    const result = mapQuestionFromServer({
+        questionId: 1,
+        title: 'test',
+        content: 'test',
+        userName: 'test',
+        created: '2022-08-01T00:00:00.000z',
+        answers: []
+    });
 
+    expect(result).toEqual({
+        questionId: 1,
+        title: 'test',
+        content: 'test',
+        userName: 'test',
+        created: new Date(Date.UTC(2021,0, 1, 0, 0, 0, 0)),
+        answers:[]
+    });
  });
