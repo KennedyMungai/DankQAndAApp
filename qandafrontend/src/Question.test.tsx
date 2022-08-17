97f06dfc8822c093b9ee55150d63fed1e433d7bd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-17 14:17:04 +0300

    Finished writing the Question react component test

--- qandafrontend/src/Question.test.tsx
+++ qandafrontend/src/Question.test.tsx
@@ -8,5 +8,27 @@ afterEach(cleanup);
 
 
 test('When the Question component is rendered, it should contain the correct data', () => { 
-    
+    const question: QuestionData = {
+        questionId: 1,
+        title: 'Title test',
+        content: 'Content test',
+        userName: 'User1',
+        created: new Date(2019,1,1),
+        answers: []
+    };  
+
+    const { queryByText } = render( 
+        <BrowserRouter>
+            <Question data={question} /> 
+        </BrowserRouter>
+    );
+
+    const titleText = queryByText('Title test');
+    expect(titleText).not.toBeNull();
+    const contentText = queryByText('Content test');
+    expect(contentText).not.toBeNull();
+    const userText = queryByText(/User1/);
+    expect(userText).not.toBeNull();
+    const dateText = queryByText(/2019/);
+    expect(dateText).not.toBeNull();
  });
